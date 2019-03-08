d6897cd7cb6f1c23f44e3049d619068de6899d45
jianyuchen <chenjy0046@gmail.com>
2019-03-08 16:11:04 +0100

    Initial

---
+++ Scheduler_P_FP.c
@@ -0,0 +1,39 @@
+#include "Scheduler.h"
+#include "Led.h"
+
+volatile uint8_t BusyPrio = NUMTASKS;      /* Current priority being served             */
+
+static void ExecuteTask (Taskp t)
+{
+  /* ----------------------- INSERT CODE HERE ----------------------- */
+  t->Invoked++;
+  if (t->Flags & TRIGGERED) {
+    SetLeds (BROWN, 1);
+	_EINT();
+	t->Taskf(t->ExecutionTime); // execute task
+	_DINT();
+	SetLeds (BROWN, 0);
+  } else {
+    t->Activated = t->Invoked;
+  }
+  /* ---------------------------------------------------------------- */
+
+}
+
+void Scheduler_P_FP (Task Tasks[])
+{ 
+  /* ----------------------- INSERT CODE HERE ----------------------- */
+  uint8_t oldBP = BusyPrio; // Set BusyPrio as the priority of the currently running task (the one that was executing just before the scheduler is called)
+
+  for(BusyPrio = 0; BusyPrio < oldBP; BusyPrio++)
+  {
+    Taskp t = &Tasks[BusyPrio];
+
+	while (t->Activated != t->Invoked)
+	{
+		ExecuteTask(t);
+	}  
+  }
+  
+  /* ---------------------------------------------------------------- */
+}
